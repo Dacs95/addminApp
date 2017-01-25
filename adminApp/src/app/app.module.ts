81f1e52753587ebbe821c5885c2538a58b4e59be
Daniel Contreras <bones2995@gmail.com>
2017-01-25 17:51:13 -0600

    BotonAgregar

--- adminApp/src/app/app.module.ts
+++ adminApp/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {AngularFireModule} from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyA2L_sIFFq4Z0hxr5Qy6ZDkS1e44ktboR0",
+    authDomain: "uploadtest-44f1e.firebaseapp.com",
+    databaseURL: "https://uploadtest-44f1e.firebaseio.com",
+    storageBucket: "uploadtest-44f1e.appspot.com",
+    messagingSenderId: "263437282038"
+};
 
 @NgModule({
   declarations: [
@@ -15,7 +24,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- adminApp/src/pages/home/home.ts
+++ adminApp/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2'
+
 
 @Component({
   selector: 'page-home',
