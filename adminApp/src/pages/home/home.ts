cfc15a8cb1905a5721e56131cbaa95f703ab851b
Daniel Contreras <bones2995@gmail.com>
2017-01-25 18:33:31 -0600

    LecturaDePacientes

--- adminApp/src/pages/home/home.ts
+++ adminApp/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2'
 
 
@@ -8,9 +8,58 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2'
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+	users: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
+  	constructor(public navCtrl: NavController, public alertCtrl: AlertController, angFire: AngularFire) {
+  		this.users = angFire.database.list('/users');
+	
+  	}
 
-  }
+  	addUser(): void{
+  		let prompt = this.alertCtrl.create({
+  			title: 'Pacient info',
+  			message: 'Enter the pacient information',
+  			inputs:[
+  				{
+  					name: 'pacientName',
+  					placeholder: "Pacient Name"
+  				},
+  				{
+  					name: 'city',
+  					placeholder: "City"
+  				},
+  				{
+  					name: 'dob',
+  					placeholder: "DD/MM/YY"
+  				},
+  				{
+  					name: 'evento',
+  					placeholder: "Medical Procedure"
+  				}
+  			],
+  			buttons: [
+  				{
+  					text:"Cancel",
+  					handler: data => {
+  						console.log("Cancel clicked");
+  					}
+  				},
+  				{
+  					text:"Save",
+  					handler: data =>{
+  						this.users.push({
+  							nombre: data.pacientName,
+  							origen: data.city,
+  							dob: data.dob,
+  							evento: data.evento
+  						})
+  					}
+  				}	
+  				
+  			]
+  		});
+  		prompt.present();
+  	}
 
 }
