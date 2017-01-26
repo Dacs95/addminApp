9344b9db20066733ecb422a94e8c2a6d270b62f0
Daniel Contreras <bones2995@gmail.com>
2017-01-26 13:07:19 -0600

    BtnEdit

--- adminApp/src/pages/home/home.ts
+++ adminApp/src/pages/home/home.ts
@@ -61,7 +61,8 @@ export class HomePage {
   							nombre: data.pacientName,
   							origen: data.city,
   							dob: data.dob,
-  							evento: data.evento
+  							evento: data.evento,
+  							psw: ""
   							}  							
   						})
   					}
@@ -70,6 +71,68 @@ export class HomePage {
   			]
   		});
   		prompt.present();
+  	}
+
+  	editPatient(user){
+  		  		let prompt = this.alertCtrl.create({
+  			title: 'Pacient info',
+  			message: 'Edit the pacient information',
+  			inputs:[
+  				{
+  					name: 'pacientName',
+  					placeholder: user.datos.nombre
+  				},
+  				{
+  					name: 'city',
+  					placeholder: "City"
+  				},
+  				{
+  					name: 'dob',
+  					placeholder: "DD/MM/YY"
+  				},
+  				{
+  					name: 'evento',
+  					placeholder: "Medical Procedure"
+  				}
+  			],
+  			buttons: [
+  				{
+  					text:"Cancel",
+  					handler: data => {
+  						console.log("Cancel clicked");
+  					}
+  				},
+  				{
+  					text:"Save",
+  					handler: data =>{
+  						this.users.push({
+  							agenda: {
+  								caso: false,
+  								evento: false,
+  								histMed: false,
+  								postop: false,
+  								preparacion: false,
+  								valoracion: false
+  							},
+  							datos:{
+  							nombre: data.pacientName,
+  							origen: data.city,
+  							dob: data.dob,
+  							evento: data.evento,
+  							psw: ""
+  							}  							
+  						})
+  					}
+  				}	
+  				
+  			]
+  		});
+  		prompt.present();
+  	}
+
+  	deletePatient(user){
+
+
   	}
 
 }
