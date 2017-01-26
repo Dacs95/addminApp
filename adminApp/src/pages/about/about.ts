da43812f066e3c8a51057d25abaa2a8dcec9f7f4
Daniel Contreras <bones2995@gmail.com>
2017-01-25 20:32:17 -0600

    MostrarDoctores

--- adminApp/src/pages/about/about.ts
+++ adminApp/src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2'
 
 @Component({
   selector: 'page-about',
@@ -8,8 +8,54 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+	users: FirebaseListObservable<any>;
 
-  }
+	constructor(public navCtrl: NavController, public alertCtrl: AlertController, angFire: AngularFire) {
+		this.users = angFire.database.list('/doctors');
+  	}
+
+  	addDoctor(): void{
+  		let prompt = this.alertCtrl.create({
+  			title: 'Doctor information',
+  			message: 'Enter the doctor information',
+  			inputs:[
+
+  				{
+  					name: 'doctorName',
+  					placeholder: "Name"
+  				},
+  				{
+  					name: 'cedula',
+  					placeholder: "License"
+  				},
+  				{
+  					name: 'area',
+  					placeholder: "Speciality"
+  				}
+  			],
+  			buttons:[
+  				{
+  					text:"Cancel",
+  					handler: data =>{
+  						console.log("Cancel clicked");
+  					}
+  				},
+  				{
+  					text: "Save",
+  					handler: data =>{
+  						this.users.push({
+  							name: data.doctorName,
+  							cedula: data.cedula,
+  							area: data.area
+  						})
+  					}
+  				}
+  			]
+
+  		});
+
+  		prompt.present();
+
+  	}
 
 }
